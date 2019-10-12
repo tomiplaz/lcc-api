4b4eead8252b7615b9714579584df51a7f04af92
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-12 23:03:33 +0200

    Initialize repo.

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = require('express')()
+const { API_PORT } = process.env
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(API_PORT, () => {
+  console.log(`Server listening on port ${API_PORT}!`)
+})
+
+const Sequelize = require('sequelize')
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD
+} = process.env
+
+const sequelize = new Sequelize(
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  { host: POSTGRES_HOST, dialect: 'postgres'}
+)
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Connected to the database!'))
+  .catch(err => console.log('Error connecting to database:', err))
