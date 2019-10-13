69ee8afccc8bbadb2d62ae148e0124523881b645
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-14 00:26:58 +0200

    Define route that returns all Products.

--- index.js
+++ index.js
@@ -3,12 +3,23 @@
 const app = require('express')()
 const { API_PORT } = process.env
 
+require('/src/sequelize')
+
 app.get('/', (req, res) => {
   res.send('Hello world!')
+})
+
+app.get('/products', async (req, res) => {
+  const { Product } = require('/src/models/product')
+
+  try {
+    const products = await Product.findAll()
+    res.send(products)
+  } catch (err) {
+    console.log('Error querying for Products at GET /products:', err)
+  }
 })
 
 app.listen(API_PORT, () => {
   console.log(`Server listening on port ${API_PORT}!`)
 })
-
-require('/src/sequelize')
