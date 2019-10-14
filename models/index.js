104951ef01e2a2b28ba26b2eae881cdc39249662
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-14 23:51:09 +0200

    Refactor initializing database and add more products.

--- models/index.js
+++ models/index.js
@@ -2,26 +2,40 @@ const { sequelize } = require('/src/sequelize')
 
 const { Product } = require('./product')
 
-sequelize
-  .sync({ force: true })
-  .then(() => {
+initDb()
+
+async function initDb() {
+  try {
+    await sequelize.sync({ force: true })
+
     console.log('Tables created!')
-    Product.create({
-      name: 'Product',
-      description: 'Lorem ipsum dolor sit amet',
-      price: 8.95
-    })
-    .then(() => {
-      console.log('Product created!')
-      Product.findAndCountAll({
-        where: { name: 'Product' }
-      })
-      .then((res) => {
-        console.log('Products found:', res.count)
-        console.log('Products:', res.rows.map(row => row.dataValues.name))
-      })
-      .catch((err) => console.log('Error finding Products:', err))
-    })
-    .catch((err) => console.log('Error creating Product:', err))
-  })
-  .catch((err) => console.log('Error creating tables:', err))
+    try {
+      const productData = {
+        name: 'Product',
+        description: 'Lorem ipsum dolor sit amet',
+        price: 1
+      }
+
+      await Product.create({ ...productData, price: 9.95 })
+      await Product.create({ ...productData, price: 12.95 })
+      await Product.create({ ...productData, price: 4.95 })
+
+      console.log('Products created!')
+
+      try {
+        const result = await Product.findAndCountAll({
+          where: { name: 'Product' }
+        })
+
+        console.log('Products found:', result.count)
+        console.log('Products:', result.rows.map(row => row.dataValues.name))
+      } catch (err) {
+        console.log('Error finding Products:', err)
+      }
+    } catch (err) {
+      console.log('Error creating Product:', err)
+    }
+  } catch (err) {
+    console.log('Error creating tables:', err)
+  }
+}
