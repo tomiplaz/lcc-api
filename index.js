51e7da1ac00d121ae1358764447ce2f092dc6755
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-18 00:44:57 +0200

    Handle POST /charge to create a Stripe charge.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
+const { API_PORT, STRIPE_SECRET_KEY } = process.env
+
 const app = require('express')()
-const cors = require('cors')
-const { API_PORT } = process.env
+const stripe = require('stripe')(STRIPE_SECRET_KEY)
 
 require('/src/sequelize')
 
-app.use(cors())
+app.use(require('cors'))
+app.use(require('body-parser').text())
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
@@ -20,6 +22,22 @@ app.get('/products', async (req, res) => {
     res.send(products)
   } catch (err) {
     console.log('Error querying for Products at GET /products:', err)
+  }
+})
+
+app.post('/charge', async (req, res) => {
+  try {
+    const { status } = await stripe.charges.create({
+      amount: 4850,
+      currency: 'eur',
+      description: 'LCC charge',
+      source: req.body
+    })
+
+    res.json({ status })
+  } catch (err) {
+    console.log('Error creating a charge:', err)
+    res.status(500).send()
   }
 })
 
